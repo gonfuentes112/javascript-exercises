2bdaceb8351bebeb0e8a9c8770137bffae0d67a5
gekishou <gonfuentes112@gmail.com>
2024-10-10 00:48:37 -0300

    exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const res = (fahrenheit -32) * 5 / 9;
+  if (Number.isInteger(res)) {
+    return res;
+  }
+  return Number(res.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const res = (celsius * 9 / 5) + 32;
+  if (Number.isInteger(res)) {
+    return res;
+  }
+  return Number(res.toFixed(1));
 };
 
 // Do not edit below this line
