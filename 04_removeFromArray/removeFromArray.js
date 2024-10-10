7cd9115a268e8fcdce178aaae6dbf6274bc03b54
gekishou <gonfuentes112@gmail.com>
2024-10-09 22:41:46 -0300

    exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    if (!targets) {
+        return arr;
+    };
+    return arr.filter(item => !targets.includes(item));
 };
 
 // Do not edit below this line
