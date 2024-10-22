c66738616a3df7b3d8869eb70d9fb09b9df5ffbd
gekishou <gonfuentes112@gmail.com>
2024-10-22 17:33:25 -0300

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    function age(person) {
+        let yearOfDeath = person.yearOfDeath
+        if (!yearOfDeath) {
+            const timestamp = Date.now();
+            const date = new Date(timestamp);
+            yearOfDeath = date.getFullYear();
+        }
+        return yearOfDeath - person.yearOfBirth;
+    }
+
+    return arr.reduce((current, next) => {
+        return age(current) > age(next) ? current : next;
+    })
 
 };
+
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]));
 
 // Do not edit below this line
 module.exports = findTheOldest;
