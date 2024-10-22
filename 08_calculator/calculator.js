79916d2fea3f372b12460b082d5f5f55dbf1f2a2
gekishou <gonfuentes112@gmail.com>
2024-10-22 04:38:27 -0300

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -32,7 +32,15 @@ const power = function(base, power) {
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let res = 1;
+  for (let i = 2; i <= num; i++){
+    res *= i;
+  }
+  return res;
 	
 };
 
