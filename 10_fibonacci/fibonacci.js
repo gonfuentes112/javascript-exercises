9de062010ef3cda44794f347b810269b918b111c
gekishou <gonfuentes112@gmail.com>
2024-10-22 13:04:28 -0300

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (isNaN(Number(num)) || !isFinite(num) || num < 0){
+        return "OOPS";
+    }
+    if (typeof(num) === 'string') {
+        num = Number(num);
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1) {
+        return 1;
+    };
+    const dict = new Map;
+    dict.set(0, 1);
+    dict.set(1, 1);
+    for (let i = 2; i <= num ; i++) {
+        dict.set(i, dict.get(i-1) + dict.get(i-2));
+    }
+    return dict.get(num-1);
 
 };
 
